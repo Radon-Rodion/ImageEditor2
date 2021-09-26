753dc980c4b4997d75417d735ea3e2b45e6fec6a
Rafael Pavlov <pavel02rafeev@gmail.com>
2021-09-27 00:27:05 +0300

    GUI without image processing logic

---
+++ SPP_3/GUIController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SPP_3
+{
+    class GUIController
+    {
+        (Slider contrastSlider, Slider brightnessSlider, Slider saturationSlider) sliders;
+        Button penColorControl;
+        Button colorController;
+
+        public GUIController(Button penColorControl, (Slider contrastSlider, Slider brightnessSlider, Slider saturationSlider) sliders)
+        {
+            this.colorController = this.penColorControl = penColorControl;
+            this.sliders = sliders;
+        }
+
+        public void SliderChangesTextBox(Slider slider, TextBox box)
+        {
+            box.Text = $"{System.Convert.ToInt32(slider.Value)}";
+        }
+
+        public void TextBoxChangesSlider(Slider slider, TextBox box)
+        {
+            try
+            {
+                if (!box.Text.Equals(""))
+                    slider.Value = System.Convert.ToInt32(box.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid value!");
+            }
+        }
+
+        /// <summary>
+        /// Changes color of the colorController button and returns true if changing pen color.
+        /// </summary>
+        public bool ChangeColor(Brush newColor) //true if changing pen color; false - if brush color
+        {
+            if (colorController != null)
+            {
+                colorController.Background = newColor;
+                if (colorController == penColorControl) return true;
+            }
+            return false;
+        }
+
+        public void SliderToZero()
+        {
+            sliders.contrastSlider.Value = 0;
+            sliders.brightnessSlider.Value = 0;
+            sliders.saturationSlider.Value = 0;
+        }
+
+        public void SetColorControl(Button newColorControl)
+        {
+            if (newColorControl != colorController)
+            {
+                /*colorController.FlatStyle = FlatStyle.Popup;
+                newColorControl.FlatStyle = FlatStyle.Standard;*/
+                colorController = newColorControl;
+            }
+        }
+    }
+}
---
+++ SPP_3/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPP_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        GUIController guiControl;
+        public MainWindow()
+        {
+            InitializeComponent();
+            guiControl = new GUIController(PenColor, (ContrastSlider, BrightnessSlider, SaturationSlider));
+            foreach (UIElement el in Colors.Children)
+            {
+                if(el is Button)
+                {
+                    ((Button)el).Click += ColorButton_Click;
+                }
+            }
+        }
+
+        private void ContrastSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (guiControl == null) return;
+            guiControl.SliderChangesTextBox(ContrastSlider, ContrastText);
+            /*Instrument = -1;
+            guiControl.ChangeChromaticity((chromaticityTrack.Value / 128f) + 1);*/
+        }
+
+        private void ContrastText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (guiControl == null) return;
+            guiControl.TextBoxChangesSlider(ContrastSlider, ContrastText);
+        }
+
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (guiControl == null) return;
+            guiControl.SliderChangesTextBox(BrightnessSlider, BrightnessText);
+            /*Instrument = -2;
+            guiControl.ChangeChromaticity((chromaticityTrack.Value / 128f) + 1);*/
+        }
+
+        private void BrightnessText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (guiControl == null) return;
+            guiControl.TextBoxChangesSlider(BrightnessSlider, BrightnessText);
+        }
+
+        private void SaturationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (guiControl == null) return;
+            guiControl.SliderChangesTextBox(SaturationSlider, SaturationText);
+            /*Instrument = -3;
+            guiControl.ChangeChromaticity((chromaticityTrack.Value / 128f) + 1);*/
+        }
+
+        private void SaturationText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (guiControl == null) return;
+            guiControl.TextBoxChangesSlider(SaturationSlider, SaturationText);
+        }
+
+        private void PenWidthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (guiControl == null) return;
+            guiControl.SliderChangesTextBox(PenWidthSlider, PenWidthText);
+            /*controller.SetPenWidth(lineWidthTrack.Value);*/
+        }
+
+        private void PenWidthText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (guiControl == null) return;
+            guiControl.TextBoxChangesSlider(PenWidthSlider, PenWidthText);
+        }
+
+        private void ColorControl_Click(object sender, RoutedEventArgs e)
+        {
+            if (guiControl == null) return;
+            guiControl.SetColorControl((Button)e.OriginalSource);
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (guiControl == null) return;
+            guiControl.ChangeColor(((Button)e.OriginalSource).Background);
+        }
+    }
+}
